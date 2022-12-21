b988ebf42203e4d7d5512baba1dc6cc7ac08aaab
Philip <philip@czymmek.me>
2022-12-21 18:42:57 +0100

    unit 1.1 done ✅

--- dartmouth_c_lang/course-v1.1.c
+++ dartmouth_c_lang/course-v1.1.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-int main(void){
+int main(void){ // inline comment
+    // a for-loop the runs 5 times 
     for(int i=0; i< 5;i++){
-        printf("hello \\ / \"world\"!\n");    
+        printf("hello \\ / \"world\"!%d\n", i);    
     } 
     return 0;
+    /* this is a mutliline comment
+    spanning over multiple lines
+    */
 }
