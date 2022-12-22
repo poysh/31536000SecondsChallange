23a2917ac2bf6b3ea353a17ef2333cc2fe7d2d4f
Philip <philip@czymmek.me>
2022-12-22 22:28:29 +0100

    unit 1.3 in progress ☑️

---
+++ dartmouth_c_lang/unit1.3/unit1.3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    char letter1 = 'i', letter2 = 'n', letter3 = 'C';
+    printf ("Programming %c%c %c\n", letter1, letter2, letter3);
+    
+    double height;
+    height = 1.89;
+    printf("I am %.2lf meters tall.",height);
+    return 0;
+}
