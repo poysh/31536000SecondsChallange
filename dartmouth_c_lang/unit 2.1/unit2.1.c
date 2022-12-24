60a97a90c5486d659fe3875d70c0814e219e5da4
Philip <philip@czymmek.me>
2022-12-24 15:25:19 +0100

    unit 2.1 done ✅

--- dartmouth_c_lang/unit 2.1/unit2.1.c
+++ dartmouth_c_lang/unit 2.1/unit2.1.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 // #include "unit2.1.h"
 
+#define FREEAGE 60
+
+int hostelPrices(void){
+    int age, luggage;
+    scanf("%d%d", &age, &luggage);
+    if(age == FREEAGE){
+        printf("0");
+    } else if(age < 10){
+        printf("5");
+    } else if(luggage > 20 && age != 60){
+        printf("40");
+    } else {
+        printf("30");
+    }
+    return 0;
+}
+
 int tugOfWar(void){
     int numberOfPlayers;
     int sumTeamA = 0, sumTeamB = 0, weightA, weightB;
@@ -28,6 +45,26 @@ int tugOfWar(void){
     return 0;
 }
 
+int logicalAnd(void){
+    int sunny = 1;
+    int vacation = 1;
+    int sunAndVacation = sunny && !vacation;
+    if(sunAndVacation){
+        printf("yeaaaaah\n");
+    } else {
+        printf("too bad\n");
+    }
+    return 0;
+}
+
+int logicalOr(void){
+    int sunny = 1;
+    int vacation = 0;
+    int sunOrVacation = sunny || vacation;
+    printf("%d (0 False, all others True)\n", sunOrVacation);
+    return 0;
+}
+
 int main(void) {
     //FALSE : 0  0.0  
     //TRUE : all the other values (positive or even negative)
@@ -50,8 +87,29 @@ int main(void) {
     } else {
         printf("A is not equal to B\n");
     }
+    
+    int arr[3];
+    arr[0] = 1;
+    arr[2] = 2;
+    arr[1] = 6;
+    
+    printf("1: %d, 2: %d, 3: %d\n", arr[0], arr[1], arr[2]);
+    
+    int readValue;
+    int array[3];
+    for(int i = 0; i < 3; i++){
+        scanf("%d", &array[i]);
+        // array[i] = readValue;
+    }
+    printf("1: %d, 2: %d, 3: %d\n", array[0], array[1], array[2]);
+    
+    double arr1[2];
+    arr1[0] = 2.0;
+    arr1[1] = 6.6;
 
-    tugOfWar();
+    // tugOfWar();
+    // logicalAnd();
+    // logicalOr();
     
     int numberOfPassengers;
     double gasPrice;
