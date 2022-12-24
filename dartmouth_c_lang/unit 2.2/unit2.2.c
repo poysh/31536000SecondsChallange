911b7126348298a7c1345f7ffa36c5c2fe552096
Philip <philip@czymmek.me>
2022-12-24 23:15:24 +0100

    unit 2.2 done ✅

--- dartmouth_c_lang/unit 2.2/unit2.2.c
+++ dartmouth_c_lang/unit 2.2/unit2.2.c
@@ -52,6 +52,30 @@ int zombieInvasion(void){
     return 0;
 }
 
+int chemicalExperiment(void){
+    int tempMin, tempMax, temperature;
+    int tempIsValid = 1;
+    int end = 0;
+    scanf("%d%d", &tempMin, &tempMax);
+
+    while (!end && tempIsValid){
+        scanf("%d", &temperature);
+        end = (temperature == -999);
+
+        tempIsValid = (tempMin <= temperature && temperature <= tempMax);
+
+        if(!end){
+            if (tempIsValid){
+                printf("Nothing to report\n");
+            } else {
+                printf("Alert!\n");
+            }
+        }
+    }
+
+    return 0;
+}
+
 int main(void){
     zombieInvasion();
 }
