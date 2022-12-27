10aa8cc1a7ad9b3648647bf89136b5d15d4af22b
Philip <philip@czymmek.me>
2022-12-27 12:35:17 +0100

    unit 3.1 progress

--- dartmouth_c_lang/unit 3.1/unit3.1.c
+++ dartmouth_c_lang/unit 3.1/unit3.1.c
@@ -1,9 +1,40 @@
 #include <stdio.h>
 
-
+//function PROTOTYPE
+int sum(int, int);
+void printLine(int, char);
+void printTriangle(int, char);
 
 int main(void){
+    int a,b;
+    int result;
 
-    printf("hello, world!");
+    scanf("%d%d", &a, &b);
+    result = sum(a, b); //ARGUMENTS
+    printf("%d\n", result);
+
+    printTriangle(result, '*');
     return 0;
+}
+
+int sum(int x, int y){ //PARAMETERS
+    return x+y;
+}
+
+void printLine(int nLines, char pattern){
+    int i;
+    int j;
+    
+    for(i = 0; i < nLines; i++){
+        printf("%c", pattern);
+    }
+    printf("\n");
+}
+
+void printTriangle(int nLines, char pattern){
+    int line, count;
+    for(line = 0; line < nLines; line++){
+        count = line + 1;
+        printLine(count, pattern);
+    }
 }
