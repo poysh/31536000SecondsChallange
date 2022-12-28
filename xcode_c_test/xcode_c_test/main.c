c6aa57446a8ed7ba58103db9dcd86b904c36aa40
Philip <philip@czymmek.me>
2022-12-28 20:40:38 +0100

    unit3.1 sumOfNumbers implementation

--- xcode_c_test/xcode_c_test/main.c
+++ xcode_c_test/xcode_c_test/main.c
@@ -10,8 +10,39 @@
 double convertTemperature(double);
 double convertWeight(double);
 double convertDistance(double);
+void converter(void);
+int sumOfDigits(int);
+
+/*
+ Please write a C-program that uses a recursive function called "sumOfDigits()" to compute the sum of the digits of a number. To do so, your main function should first read an integer number as input and then call sumOfDigits(), which should in turn call itself until there are no digits left to sum, at which point the final sum should display to the user.
+
+ Here is the main idea of how the recursion in sumOfDigits() should work:
+
+ sumOfDigits(6452) = 2 + sumOfDigits(645)
+
+ sumOfDigits(645) = 5 + sumOfDigits(64)
+ */
 
 int main(int argc, const char * argv[]) {
+    int number;
+    
+    scanf("%d", &number);
+    printf("%d", sumOfDigits(number));
+}
+
+int sumOfDigits(int n){
+    if (n < 0){
+        printf("%d is a negative number", n);
+        return -1;
+    } else if (n == 0) {
+        return 0; // base case: if there are no digits left, return 0
+    } else {
+        return (n % 10) + sumOfDigits(n/10);
+    }
+}
+
+
+void converter(){
     int nConversion;
     double num;
     char unit;
@@ -37,8 +68,6 @@ int main(int argc, const char * argv[]) {
             printf("Conversion not possible\n");
         }
     }
-    
-    return 0;
 }
 
 // temperature in degrees Fahrenheit = 32 + 1.8 × temperature in degrees Celsius.
