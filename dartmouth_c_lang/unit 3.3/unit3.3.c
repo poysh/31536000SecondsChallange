008ee76baeba49d0010aa33000a84ef991b3a7d7
Philip <philip@czymmek.me>
2022-12-30 19:29:12 +0100

    unit 3.3 completed ✅

--- dartmouth_c_lang/unit 3.3/unit3.3.c
+++ dartmouth_c_lang/unit 3.3/unit3.3.c
@@ -2,6 +2,9 @@
 
 void getPointerAddress();
 void dereferencePointer();
+void swapVariable();
+void reset(int *);
+void resetArrayNotation(int []);
 
 void getPointerAddress(){
     int a = 42;
@@ -25,8 +28,58 @@ void dereferencePointer(){
     printf("Address: %p, value at address: %d\n", addressOfA, *addressOfA);
 }
 
+void swapVariables(int * a, int * b){
+    int tmp = * a;
+    * a = * b;
+    * b = tmp;
+}
+
+int arrayAddress(){
+    int arr[3] = {15, 16, 17};
+    for(int i = 0; i < 3; i++){
+        printf("%d\n", arr[i]);
+    }
+    printf("%p\n",arr);
+    int * ptr = arr;
+    * ptr = 2;
+    * (ptr + 1) = 3; //pointer arithmetic
+    for(int i = 0; i < 3; i++){
+        printf("%d\n", arr[i]);
+    }
+    return 0;
+}
+
+
+void setup() {
+    int arr[3] = {15, 16, 17};
+    reset(arr);
+    arr[0] = 15;
+    arr[1] = 16;
+    arr[2] = 17;
+    resetArrayNotation(arr);
+}
+
+void reset(int * ptr){
+    *(ptr + 0) = 0;
+    *(ptr +1) = 0;
+    *(ptr +2) = 0;
+}
+
+void resetArrayNotation(int ptr[]){
+    ptr[0] = 0;
+    ptr[1] = 0;
+    ptr[2] = 0;
+}
+
 int main(){
     getPointerAddress();
     dereferencePointer();
+    arrayAddress();
+    setup();
+
+    int a = 24;
+    int b = 42;
+    swapVariables( &a, &b);
+    printf("a: %d, b: %d", a, b);
     return 0;
 }
