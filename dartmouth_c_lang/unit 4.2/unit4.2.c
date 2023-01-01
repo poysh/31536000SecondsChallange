0237a6b96a29c09105dd3b60ec6be19ad824b027
Philip <philip@czymmek.me>
2023-01-01 23:04:36 +0100

    unit 4.3 started ☑️

--- dartmouth_c_lang/unit 4.2/unit4.2.c
+++ dartmouth_c_lang/unit 4.2/unit4.2.c
@@ -4,6 +4,7 @@
 void multiDimesonalArrays();
 void setToZero(short**);
 void setup();
+void strings();
 
 int main()
 {
@@ -45,4 +46,21 @@ void setToZero(short** t)
     *((*t) + 2) = 0; // t[0][2]  OR *(t[0] + 2)
     *(*(t + 1)) = 0; // t[1][0]  OR *(t[1] + 0)
     *(*(t + 1) + 1) = 0; // t[1][1]  OR *(t[1] + 1)
+}
+
+void strings()
+{
+    char a[4];
+    char b[6];
+    char c[9];
+    char* words[3] = { a, b, c };
+    printf("Please enter a word with at  most 3 letters: ");
+    scanf("%s", a);
+    printf("Please enter a word with at  most 5 letters: ");
+    scanf("%s", b);
+    printf("Please enter a word with at  most 8 letters: ");
+    scanf("%s", c);
+    printf("You entered: \n");
+    printf("%s %s %s.\n", a, b, c);
+    printf("%s %s %s.\n", words[0], words[1])
 }
