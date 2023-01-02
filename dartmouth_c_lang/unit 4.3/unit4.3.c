a79a08ac4f99bd2c840d14f5f182c0ffa12ca41f
Philip <philip@czymmek.me>
2023-01-02 11:45:56 +0100

    unit 4.3 ✅ unit 5.1 ☑️

--- dartmouth_c_lang/unit 4.3/unit4.3.c
+++ dartmouth_c_lang/unit 4.3/unit4.3.c
@@ -2,15 +2,52 @@
 #include <stdlib.h>
 
 void mallocFreeAction();
+int* allocateIntArray(int);
+void dynamicArrays();
+double findAverage(int*, int);
+
+int* allocateIntArray(int size)
+{
+    return ((int*)malloc(size * sizeof(int)));
+}
+
+void dynamicArrays()
+{
+    int num, i;
+    int* array;
+    double average;
+    printf("How many grades would you like to enter? ");
+    scanf("%d", &num);
+    array = allocateIntArray(num);
+    printf("Please enter %d grades: ", num);
+    for (i = 0; i < num; i++) {
+        scanf("%d", &array[i]);
+    }
+    average = findAverage(array, num);
+    printf("%.2lf\n", average);
+    for (i = 0; i < num; i++) {
+        printf("Grade %d: %d\n", i + 1, array[i]);
+    }
+    free(array);
+}
+
+double findAverage(int* ptr, int size)
+{
+    double sum = 0.0;
+    for (int i = 0; i < size; i++) {
+        sum += ptr[i];
+    }
+    return sum / size;
+}
 
 void mallocFreeAction()
 {
-    int * intptr;
-    double * doubleptr;
-    intptr = (int *) malloc(sizeof(int));
-    * intptr = 5;
-    doubleptr = (double *) mallac(sizeof(double));
-    * doubleptr = 9.42;
+    int* intptr;
+    double* doubleptr;
+    intptr = (int*)malloc(sizeof(int));
+    *intptr = 5;
+    doubleptr = (double*)malloc(sizeof(double));
+    *doubleptr = 9.42;
     int* p1 = malloc(4 * sizeof(int)); // allocates enough for an array of 4 int
     int* p2 = malloc(sizeof(int[4])); // same, naming the type directly
     int* p3 = malloc(4 * sizeof *p3); // same, without repeating the type name
@@ -31,6 +68,7 @@ void mallocFreeAction()
 
 int main()
 {
+    dynamicArrays();
     mallocFreeAction();
     return 0;
 }
