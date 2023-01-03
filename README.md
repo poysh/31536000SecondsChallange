8e644e2c33d5e9a6f604b79511dd96536a3c063f
Philip <philip@czymmek.me>
2023-01-03 21:24:46 +0100

    unit 5.2 - linked Lists with structs and pointers

--- README.md
+++ README.md
@@ -4,5 +4,12 @@
 Day 1: Initial setup and deciding on a project.
 
 
-Always run clang-formater to format code
+## Always run clang-formater to format code
+
 clang-format -i -style=WebKit *.c *.h
+
+## Compile code with Clang
+clang <file.c>
+
+## Run program with the following command
+./a.out
--- linkedList/linkedList/main.c
+++ linkedList/linkedList/main.c
@@ -1,10 +1,12 @@
+//
+//  main.c
+//  linkedList
+//
+//  Created by Philip Czymmek on 03.01.23.
+//
+
 #include <stdio.h>
 #include <stdlib.h>
-
-struct point {
-    int x;
-    int y;
-};
 
 struct linkedPoint {
     int x;
@@ -12,24 +14,13 @@ struct linkedPoint {
     struct linkedPoint* next;
 };
 
-void readPoint(struct point* ptr);
-void printPoint(struct point pt);
-void printPoly(struct point* ptr, int verticies);
 void printPoints(struct linkedPoint* start);
 struct linkedPoint* append(struct linkedPoint* end, struct linkedPoint* newpt);
 struct linkedPoint* createPoint(int x, int y);
 void freePoints(struct linkedPoint* start);
 
-int main()
+int main(int argc, const char * argv[])
 {
-    int i, num;
-    struct point z;
-    struct point* polygon;
-
-    // struct linkedPoint pt1 = { 1, 2, NULL };
-    // struct linkedPoint pt2 = { 2, 3, NULL };
-    // struct linkedPoint pt3 = { 3, 1, NULL };
-
     struct linkedPoint *start, *end, *newpt;
     int j, k;
     int x, y;
@@ -42,52 +33,16 @@ int main()
         printf("y = ");
         scanf("%d", &y);
         newpt = createPoint(x, y);
-        if (i == 0) {
+        if (k == 0) {
             start = end = newpt;
         } else {
             end = append(end, newpt);
         }
     }
-
-    // start = end = &pt1; // right to left assignment
-    // end = append(end, &pt2); // & reference to address
-    // end = append(&pt2, &pt3);
-    // pt1.next = &pt2; // access field and write new value to it
-    // pt2.next = &pt3;
-
+    
     printPoints(start);
     freePoints(start);
-
-    printf("How many vertices does your polygon have? ");
-    scanf("%d", &num);
-    polygon = (struct point*)malloc(num * sizeof(struct point));
-    // struct point triangle[3];
-    for (i = 0; i < num; i++) {
-        readPoint(&polygon[i]);
-    }
-    printPoly(polygon, num);
-    readPoint(&z);
-    printPoint(z);
-
-    free(polygon);
     return 0;
-}
-
-void readPoint(struct point* ptr)
-{
-    scanf("%d %d", &ptr->x, &ptr->y);
-}
-
-void printPoint(struct point pt)
-{
-    printf("x: %d, y: %d\n", pt.x, pt.y);
-}
-
-void printPoly(struct point* ptr, int verticies)
-{
-    for (int i = 0; i < verticies; i++) {
-        printPoint(ptr[i]);
-    }
 }
 
 void printPoints(struct linkedPoint* start)
