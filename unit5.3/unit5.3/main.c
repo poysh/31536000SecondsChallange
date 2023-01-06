70d7f0cadc72fba96e1f9e3fef1ee6c24d0b430a
Philip <philip@czymmek.me>
2023-01-06 20:59:44 +0100

    implemented artes school knowledge

--- unit5.3/unit5.3/main.c
+++ unit5.3/unit5.3/main.c
@@ -18,12 +18,17 @@ struct digit* append(struct digit * pt1, struct digit * pt2);
 void freeList(struct digit * start);
 void printList(struct digit * start);
 struct digit* readNumber(void);
+int divisibleByThree(struct digit * start);
 
 int main(int argc, const char* argv[])
 {
     struct digit *start;
     start = readNumber();
     printList(start);
+    if (divisibleByThree(start))
+            printf("is divisible by 3.\n");
+        else
+            printf("is not divisible by 3.\n");
     freeList(start);
 
     return 0;
@@ -82,4 +87,20 @@ struct digit* readNumber()
         scanf("%c", &c);
     }
     return start;
+}
+
+int divisibleByThree(struct digit * start)
+{
+    struct digit * tmp = start;
+    int sum = 0;
+    while(tmp!=NULL){
+        sum += tmp->num;
+        tmp = tmp->next;
+    }
+    printf("%d", sum);
+    if (sum%3 == 0){
+        return 1;
+    } else {
+        return 0;
+    }
 }
