5d3b11d6d662b9b414d26ae73db41a0b69261589
Philip <philip@czymmek.me>
2023-01-24 20:24:08 +0100

    body - multiple parameters

--- fastAPI/path_param_num_validation.py
+++ fastAPI/path_param_num_validation.py
@@ -21,3 +21,22 @@ async def read_tricky_items(*, item_id: int = Path(title="The ID of the item to 
     if q:
         results.update({"q": q})
     return results
+
+
+#     gt: greater than
+#     ge: greater than or equal
+#     lt: less than
+#     le: less than or equal
+@app.get("/items3/{item_id}")
+async def get_num_valid(
+        *,
+        item_id: int = Path(title="The ID of the item to get", ge=0, le=1000),
+        q: str,
+        size: float = Query(gt=0, lt=10.5)
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    if size:
+        results.update({"size": size})
+    return results
