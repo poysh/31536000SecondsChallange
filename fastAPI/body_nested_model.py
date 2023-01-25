8438d9231a7b8b897df743c84af4219a0dfe2e3a
Philip <philip@czymmek.me>
2023-01-25 20:25:11 +0100

    started nested models

---
+++ fastAPI/body_nested_model.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, Body
+from pydantic import BaseModel
+
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+    tags: set[str] = set()
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item = Body(embed=True)):
+    results = {"item_id": item_id, "item": item}
+    return results
