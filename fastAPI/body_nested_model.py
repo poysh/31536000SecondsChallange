8e7e22a37110ba58132c20a25115f85048373dbc
Philip <philip@czymmek.me>
2023-01-26 19:08:38 +0100

    nested body with Image

--- fastAPI/body_nested_model.py
+++ fastAPI/body_nested_model.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI, Body
 from pydantic import BaseModel
 
-
 app = FastAPI()
+
+
+class Image(BaseModel):
+    url: str
+    name: str
 
 
 class Item(BaseModel):
@@ -11,8 +15,21 @@ class Item(BaseModel):
     price: float
     tax: float | None = None
     tags: set[str] = set()
+    image: Image | None = None
 
 
+# Expected body
+# {
+#     "name": "Foo",
+#     "description": "The pretender",
+#     "price": 42.0,
+#     "tax": 3.2,
+#     "tags": ["rock", "metal", "bar"],
+#     "image": {
+#         "url": "http://example.com/baz.jpg",
+#         "name": "The Foo live"
+#     }
+# }
 @app.put("/items/{item_id}")
 async def update_item(item_id: int, item: Item = Body(embed=True)):
     results = {"item_id": item_id, "item": item}
