b3f2ba65ec6e338bc2773a988955958f06c35302
Philip <philip@czymmek.me>
2023-01-27 21:30:16 +0100

    cookies and headers

--- fastAPI/extra_data_types.py
+++ fastAPI/extra_data_types.py
@@ -14,14 +14,14 @@ async def read_items(
         repeat_at: time | None = Body(default=None),
         process_after: timedelta | None = Body(default=None),
 ):
-    start_process = start_datetime + process_after
-    duration = end_datetime - start_process
-    return {
-        "item_id": item_id,
-        "start_datetime": start_datetime,
-        "end_datetime": end_datetime,
-        "repeat_at": repeat_at,
-        "process_after": process_after,
-        "start_process": start_process,
-        "duration": duration,
-    }
+    results = {"item_id": item_id}
+    if start_datetime:
+        start_process = start_datetime + process_after
+        duration = end_datetime - start_process
+        results.update({"start_datetime": start_datetime,
+                        "end_datetime": end_datetime,
+                        "repeat_at": repeat_at,
+                        "process_after": process_after,
+                        "start_process": start_process,
+                        "duration": duration})
+    return results
