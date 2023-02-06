36418504c24819068cc8031432783de225999ddb
Philip <philip@czymmek.me>
2023-02-06 21:19:32 +0100

    not very usuful commit

--- fastAPI/sql_app/main.py
+++ fastAPI/sql_app/main.py
@@ -18,6 +18,11 @@ def get_db():
         db.close()
 
 
+@app.get("/")
+async def hello_world():
+    return {"message": "successful"}
+
+
 @app.post("/users/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
